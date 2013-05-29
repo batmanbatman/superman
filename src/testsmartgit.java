f12864ffe214848b960558b423c96c8c0d3e6ceb
Tom <ramtom@web.de>
2013-05-29 11:27:36 +0200

    asdfghjklö

--- src/testsmartgit.java
+++ src/testsmartgit.java
@@ -2,6 +2,14 @@
 public class testsmartgit {
 
  int bla = 0;
+ int blfdsa = 0;
+ 
+ int bl5a = 0;
+ 
+ int b3la = 0;
+ 
+ int b2la = 0;
+ 
  
  public void method (){
 	 
@@ -10,5 +18,8 @@ public class testsmartgit {
  
  public static void branch1 (int a, int b){
 	 
+	 //fsdafsdfgjsdjnfjasdklfklasdf
+	 
+	 int c;
  }
 }
